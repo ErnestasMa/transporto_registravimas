9cf39c163036ca7ae959d23dd52395ea30e794df
Ernestas Maleckas <maleckas.ernestas@gmail.com>
2020-05-03 19:29:30 +0300

    Implemented authentication, login screen and sample for inserting and getting of data in firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,20 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 import 'package:flutter/material.dart';
-import 'package:transporto_registravimas/screens/home.dart';
+import 'package:transporto_registravimas/services/authentication.dart';
+import 'package:transporto_registravimas/pages/root_page.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        home: Home()
-    );
+        title: 'Transporto priemonių registravimo elektroninė paslauga',
+        debugShowCheckedModeBanner: false,
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new RootPage(auth: new Auth()));
   }
 }
